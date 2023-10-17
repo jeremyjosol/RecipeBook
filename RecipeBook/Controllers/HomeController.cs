15eddc9e655978081c05b781d763ad1528bf5d41
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-17 11:36:28 -0700

    Update Home + create Account Views

--- RecipeBook/Controllers/HomeController.cs
+++ RecipeBook/Controllers/HomeController.cs
@@ -18,10 +18,23 @@ namespace RecipeBook.Controllers
         _userManager = userManager;
         _db = db;
       }
-    
-    public ActionResult Index()
-    {
-      return View();
-    }
+
+        [HttpGet("/")]
+        public async Task<ActionResult> Index()
+        {
+            Category[] cats = _db.Categories.ToArray();
+            Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+            model.Add("categories", cats);
+            string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+            if (currentUser != null)
+            {
+                Recipe[] recipes = _db.Recipes
+                    .Where(entry => entry.User.Id == currentUser.Id)
+                    .ToArray();
+                    model.Add("recipes", recipes);
+            }
+        return View(model);
+        }
     }
 }
