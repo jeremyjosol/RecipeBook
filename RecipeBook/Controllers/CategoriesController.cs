c0ba4aeb2e6b3a21360e72739bc77a6a36e406d1
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-17 10:54:37 -0700

    Full CategoriesController CRUD + corresponding Views

---
+++ RecipeBook/Controllers/CategoriesController.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using RecipeBook.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeBook.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly RecipeBoxContext _db;
+
+    public CategoriesController(RecipeBoxContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Category> model = _db.Categories.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Category category)
+    {
+      _db.Categories.Add(category);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Category thisCategory = _db.Categories
+                                 .Include(category => category.Recipes)
+                                 .ThenInclude(item => item.JoinEntities)
+                                 .ThenInclude(join => join.Tag)
+                                 .FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+    [HttpPost]
+    public ActionResult Edit(Category category)
+    {
+      _db.Categories.Update(category);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+    
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      _db.Categories.Remove(thisCategory);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
