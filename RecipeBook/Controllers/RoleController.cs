32eea0af6bb65a2b761fd50a515e0e58104b5d0e
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-18 12:44:56 -0700

    Clean up code :sponge: :soap: :sparkles:

--- RecipeBook/Controllers/RoleController.cs
+++ RecipeBook/Controllers/RoleController.cs
@@ -12,108 +12,88 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RecipeBook.Controllers
 {
- public class RoleController : Controller  
+  public class RoleController : Controller  
+  {  
+    private readonly RoleManager<IdentityRole> _roleManager;  
+    private readonly UserManager<ApplicationUser> _userManager;  
+    public RoleController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)  
     {  
-        private readonly RoleManager<IdentityRole> _roleManager;  
-        private readonly UserManager<ApplicationUser> _userManager;  
-        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)  
-        {  
-            _roleManager = roleManager;  
-            _userManager = userManager;  
-        }
+      _roleManager = roleManager;  
+      _userManager = userManager;  
+    }
 
-        [NonAction]
-        public async Task<bool> IsUserInRole(ApplicationUser user, string roleName)
-        {
-          return await _userManager.IsInRoleAsync(user, roleName);
-        }
+    private void Errors(IdentityResult result)
+    {
+      foreach (IdentityError error in result.Errors)
+      ModelState.AddModelError("", error.Description);
+    }
 
-        // public ViewResult Index()
-        // {
-        //   ViewBag.Roles = _roleManager.Roles.ToList();
-        //   ViewBag.Users = _userManager.Users.ToList();
-    
-        //   return View();
-        // }
+    public ViewResult Index()
+    {
+      List<IdentityRole> roles = _roleManager.Roles.ToList();
+      Dictionary<IdentityRole, List<ApplicationUser>> usersWithRole = new Dictionary<IdentityRole, List<ApplicationUser>>();
 
-        public ViewResult Index()
-        {
-          List<IdentityRole> roles = _roleManager.Roles.ToList();
-          Dictionary<IdentityRole, List<ApplicationUser>> usersWithRole = new Dictionary<IdentityRole, List<ApplicationUser>>();
+      foreach (IdentityRole role in roles)
+      {
+        List<ApplicationUser> usersInRole = _userManager.GetUsersInRoleAsync(role.Name).Result.ToList();
+        usersWithRole.Add(role, usersInRole);
+      }
+      return View(usersWithRole);
+    }
 
-          foreach (IdentityRole role in roles)
-          {
-            List<ApplicationUser> usersInRole = _userManager.GetUsersInRoleAsync(role.Name).Result.ToList();
-            usersWithRole.Add(role, usersInRole);
-          }
-
-          return View(usersWithRole);
-        }
-
-
-
-        private void Errors(IdentityResult result)
-        {
-          foreach (IdentityError error in result.Errors)
-          ModelState.AddModelError("", error.Description);
-        }
-
-
-        public IActionResult Create() => View();
-
-        [HttpPost]
-        public async Task<IActionResult> Create([Required] string name)
-        {
-          if (ModelState.IsValid)
-          {
-            IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
-            if (result.Succeeded)
-            return RedirectToAction("Index");
-              else
-                Errors(result);
-          }
+    public IActionResult Create() => View();
+      
+    [HttpPost]
+    public async Task<IActionResult> Create([Required] string name)
+    {
+      if (ModelState.IsValid)
+      {
+        IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+        if (result.Succeeded)
+        return RedirectToAction("Index");
+        else
+        Errors(result);
+      }
       return View(name);
     }
         
-        [HttpGet]
-        public IActionResult Assign()
-        {
-          List<IdentityRole> roles = _roleManager.Roles.ToList();
-          List<ApplicationUser> users = _userManager.Users.ToList();
-          ViewBag.RolesList = new SelectList(roles, "Name", "Name");
-          ViewBag.UsersList = new SelectList(users, "Id", "UserName");
-
-          return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Assign(string UserId, string RoleName)
-        {
-          try
-          {
-            if (!string.IsNullOrEmpty(UserId) && !string.IsNullOrEmpty(RoleName))
-            {
-              ApplicationUser user = await _userManager.FindByIdAsync(UserId);
-                if (user != null)
-                {
-                  if (await _roleManager.RoleExistsAsync(RoleName))
-                  {
-                    if (!await _userManager.IsInRoleAsync(user, RoleName))
-                    {
-                      await _userManager.AddToRoleAsync(user, RoleName);
-                      return RedirectToAction("Index");
-                    }
-                  }
-                }
-              }
-            return View();
-          }
-          catch
-          {
-            return View();
-          }
-        }
-
+    [HttpGet]
+    public IActionResult Assign()
+    {
+      List<IdentityRole> roles = _roleManager.Roles.ToList();
+      List<ApplicationUser> users = _userManager.Users.ToList();
+      ViewBag.RolesList = new SelectList(roles, "Name", "Name");
+      ViewBag.UsersList = new SelectList(users, "Id", "UserName");
+      return View();
     }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Assign(string UserId, string RoleName)
+    {
+      try
+      {
+        if (!string.IsNullOrEmpty(UserId) && !string.IsNullOrEmpty(RoleName))
+        {
+          ApplicationUser user = await _userManager.FindByIdAsync(UserId);
+          if (user != null)
+          {
+            if (await _roleManager.RoleExistsAsync(RoleName))
+            {
+              if (!await _userManager.IsInRoleAsync(user, RoleName))
+              {
+                await _userManager.AddToRoleAsync(user, RoleName);
+                return RedirectToAction("Index");
+              }
+            }
+          }
+        }
+        return View();
+      }
+      catch
+      {
+        return View();
+      }
+    }
+  }
 }
